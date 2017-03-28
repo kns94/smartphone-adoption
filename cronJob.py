01b1b48ee2d377e4a1d0fd8f4ce4dd6483cbb925
Kushal Shah <kns971@gmail.com>
2017-03-27 23:09:27 -0600

    Update cronJob.py

--- cronJob.py
+++ cronJob.py
@@ -1,6 +1,6 @@
 """Creating a cron job to parse through database everyday"""
 
-import os
+import os, sys
 from crontab import CronTab
 
 class cronJob:
@@ -23,17 +23,17 @@ class cronJob:
             return True
 
 
-def main():
+def main(ccn_db, output_dir, hour, minute):
     """Commands to set up a cron job"""
 
 
     python_path = '/usr/bin/python2.7' #Set your python path 
     file_path = "{}/{}".format(os.getcwd(), 'fetchSubscriberTAC.py') #The fetchSubscriberTAC.py file should be in the same directory as this cron scheduler
 
-    args = "{} {} {} {} {} {}".format("-f", os.getcwd(), "-c", "ccn_db", "-o", "tac_records") #Set you command line arguments accordingly
+    args = "{} {} {} {} {} {}".format("-f", os.getcwd(), "-c", ccn_db, "-o", output_dir) #Set you command line arguments accordingly
     command = "{} {} {}".format(python_path, file_path, args)
 
-    cronJob().add_daily(command, 13, 05)
+    cronJob().add_daily(command, int(hour), int(minute))
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
